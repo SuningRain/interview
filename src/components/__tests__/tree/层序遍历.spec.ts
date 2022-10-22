6cd7b4ead5c1f3e46bf0f53ea236887525fabc51
ZhangYu <2783789733@qq.com>
2022-10-22 16:45:26 +0800

    feat: 二叉树的层序遍历

---
+++ src/components/__tests__/tree/层序遍历.spec.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "vitest";
+import { TreeNode } from '../../suanfa/tree'
+import { levelOrder, levelOrder2 } from '../../suanfa/二叉树层序遍历'
+
+describe('二叉树层序遍历-递归方式', () => {
+  let t = new TreeNode(1)
+  let t1 = new TreeNode(2)
+  t.left = t1
+  it('正常例子', () => {
+    expect(levelOrder(t)).toEqual([[1], [2]])
+  })
+})
+
+describe('二叉树层序遍历-队列方式', () => {
+  let t = new TreeNode(1)
+  let t1 = new TreeNode(2)
+  t.left = t1
+  it('正常例子', () => {
+    expect(levelOrder(t)).toEqual([[1], [2]])
+  })
+})
---
+++ src/components/suanfa/二叉树层序遍历.ts
@@ -0,0 +1,48 @@
+import type { TreeNode } from './tree'
+
+/**
+ * 层序遍历-递归方式
+ * @param root TreeNode类
+ * @return int整型二维数组
+ */
+export function levelOrder(root: TreeNode | null): number[][] {
+  let res: any[][] = []
+  function innderLevelOrder(root: TreeNode | null, level: number) {
+    if (!root) return
+    if (level >= res.length) {
+      res.push([])
+    }
+    res[level].push(root.val)
+    innderLevelOrder(root.left, level + 1)
+    innderLevelOrder(root.right, level + 1)
+  }
+  innderLevelOrder(root, 0)
+  return res
+}
+
+
+/**
+ * 层序遍历-队列方式
+ * @param root TreeNode类
+ * @returns int整型二维数组
+ */
+export function levelOrder2(root: TreeNode): number[][] {
+  // write code here
+  if (!root) return []
+  let queue = [root]
+  let res = []
+  while (queue.length) {
+    let rowData = []
+    for (const top of queue) {
+      rowData.push(top.val)
+      if (top.left) {
+        queue.push(top.left)
+      }
+      if (top.right) {
+        queue.push(top.right)
+      }
+    }
+    if (rowData.length) res.push(rowData)
+  }
+  return res
+}
