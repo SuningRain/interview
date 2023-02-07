9c484645d6cebc76a02654a9f5d53def09f90b48
ZhangYu <2783789733@qq.com>
2023-02-07 18:53:40 +0800

    refact: 手写instanceOf

--- src/components/webBase/JavaScript篇/J12-数据类型/index.js
+++ src/components/webBase/JavaScript篇/J12-数据类型/index.js
@@ -1,7 +1,28 @@
+// 判断数据类型
 export function getType (value) {
   const type = typeof value
   if (type !== 'object') {
     return type
   }
   return Object.prototype.toString.call(data).replace(/^[object (\S+)]$/, '$1')
+}
+
+// 手写instanceOf
+export function myInstanceOf (leftValue, rightValue) {
+  if (typeof rightValue !== 'function') {
+    console.error(`${rightValue} is not a function`)
+    return false
+  }
+  leftValue = Object.getPrototypeOf(leftValue)
+  const rightProto = rightValue.prototype
+
+  while (true) {
+    if (leftValue === null) {
+      return false
+    }
+    if (leftValue === rightProto) {
+      return true
+    }
+    leftValue = Object.getPrototypeOf(leftValue)
+  }
 }
